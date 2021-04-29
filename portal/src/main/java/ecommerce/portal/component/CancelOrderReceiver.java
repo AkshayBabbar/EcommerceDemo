69eedff2c81b871d4d8b626654e5442e54d6cfd8
Akshay <akshay.babbar@publicissapient.com>
2021-04-29 23:35:51 +0530

    init for Home Controller

--- portal/src/main/java/ecommerce/portal/component/CancelOrderReceiver.java
+++ portal/src/main/java/ecommerce/portal/component/CancelOrderReceiver.java
@@ -1,8 +1,11 @@
 package ecommerce.portal.component;
 
+import ecommerce.portal.service.OmsPortalOrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,5 +13,15 @@ import org.springframework.stereotype.Component;
 public class CancelOrderReceiver {
 
     private static Logger LOGGER = LoggerFactory.getLogger(CancelOrderReceiver.class);
+
+    @Autowired
+    private OmsPortalOrderService omsPortalOrderService;
+
+    @RabbitHandler
+    private void handle(long orderId) {
+        omsPortalOrderService.cancelOrder(orderId);
+        LOGGER.info("Process OrderID : {}",orderId);
+    }
+
 
 }
--- portal/src/main/java/ecommerce/portal/service/OmsPortalOrderService.java
+++ portal/src/main/java/ecommerce/portal/service/OmsPortalOrderService.java
@@ -1,6 +1,10 @@
 package ecommerce.portal.service;
 
+import org.springframework.transaction.annotation.Transactional;
+
 public interface OmsPortalOrderService {
+    @Transactional
+    void cancelOrder(long orderId);
 
 //ConfirmOrderResult generateConfirmOrder(List<Long> cartIds);
 }
