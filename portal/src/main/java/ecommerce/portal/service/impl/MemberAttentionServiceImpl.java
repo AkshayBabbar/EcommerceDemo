d2fc0f64fe2aa0e5b3053d62a5887f8c30f9900b
Akshay <akshay.babbar@publicissapient.com>
2021-05-12 00:01:27 +0530

    Added MemberAttentionServiceImpl and added the remaining config

---
+++ portal/src/main/java/ecommerce/portal/service/impl/MemberAttentionServiceImpl.java
@@ -0,0 +1,56 @@
+package ecommerce.portal.service.impl;
+
+import ecommerce.ormmapper.model.UmsMember;
+import ecommerce.portal.domain.MemberBrandAttention;
+import ecommerce.portal.repository.MemberBrandAttentionRepository;
+import ecommerce.portal.service.MemberAttentionService;
+import ecommerce.portal.service.UmsMemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+public class MemberAttentionServiceImpl implements MemberAttentionService {
+
+    @Autowired
+    private MemberBrandAttentionRepository memberBrandAttentionRepository;
+
+    @Autowired
+    private UmsMemberService memberService;
+
+
+
+
+    @Override
+    public int subscribe(MemberBrandAttention memberBrandAttention) {
+        int count = 0;
+        UmsMember member = memberService.getCurrentMember();
+        memberBrandAttention.setMemberId(member.getId());
+        memberBrandAttention.setMemberNickname(member.getNickname());
+        memberBrandAttention.setMemberIcon(member.getIcon());
+        memberBrandAttention.setCreateTime(new Date());
+        MemberBrandAttention findAttention = memberBrandAttentionRepository.findByMemberIdAndBrandId(memberBrandAttention.getMemberId(), memberBrandAttention.getBrandId());
+        if (findAttention == null) {
+            memberBrandAttentionRepository.save(memberBrandAttention);
+            count = 1;
+        }
+        return count;
+    }
+
+    @Override
+    public int unsubscribe(Long brandId) {
+        UmsMember member = memberService.getCurrentMember();
+        return memberBrandAttentionRepository.deleteByMemberIdAndBrandId(member.getId(),brandId);
+    }
+
+    @Override
+    public MemberBrandAttention detail(Long brandId) {
+        UmsMember member = memberService.getCurrentMember();
+        return memberBrandAttentionRepository.findByMemberIdAndBrandId(member.getId(), brandId);
+    }
+
+    @Override
+    public void clear() {
+        UmsMember member = memberService.getCurrentMember();
+        memberBrandAttentionRepository.deleteAllByMemberId(member.getId());
+    }
+}
